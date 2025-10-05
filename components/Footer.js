aae4316e7a4d04507cd8632d3ba0a61a913176e2
Jason Vance <jbvance@gmail.com>
2025-10-05 07:05:32 -0500

    small change to Footer to trigger new deployment to Vercel

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-light border-top py-3 mt-auto">
       <div className="container text-center text-muted small">
-        &copy; {new Date().getFullYear()} Linxify — All rights reserved.
+        &copy; {new Date().getFullYear()} Linxify —-- All rights reserved.
       </div>
     </footer>
   );
